2561e26f821f3a9cd67b870da858bac2517e3e1a
Donatas Noreika <donoras@gmail.com>
2023-01-31 08:28:53 +0200

    10: +Padaryti, kad profilio puslapyje būtų galima redaguoti vartotojo informaciją, pakeisti nuotrauką.

--- mysite/autoservice/views.py
+++ mysite/autoservice/views.py
@@ -7,7 +7,7 @@ from django.contrib.auth.forms import User
 from django.views.decorators.csrf import csrf_protect
 from django.contrib import messages
 from django.views.generic.edit import FormMixin
-from .forms import UzsakymasKomentarasForm
+from .forms import UzsakymasKomentarasForm, UserUpdateForm, ProfilisUpdateForm
 from django.contrib.auth.decorators import login_required
 
 # Create your views here.
@@ -126,4 +126,20 @@ def register(request):
 
 @login_required
 def profilis(request):
-    return render(request, 'profilis.html')
+    if request.method == "POST":
+        u_form = UserUpdateForm(request.POST, instance=request.user)
+        p_form = ProfilisUpdateForm(request.POST, request.FILES, instance=request.user.profilis)
+        if u_form.is_valid() and p_form.is_valid():
+            u_form.save()
+            p_form.save()
+            messages.success(request, f"Profilis atnaujintas")
+            return redirect('profilis')
+    else:
+        u_form = UserUpdateForm(instance=request.user)
+        p_form = ProfilisUpdateForm(instance=request.user.profilis)
+
+    context = {
+        'u_form': u_form,
+        'p_form': p_form,
+    }
+    return render(request, 'profilis.html', context)
